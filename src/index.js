2c2dc4bade3ff39814c5e0bc5c18e30babc44a95
winovation <gwon.innovation@gmail.com>
2021-10-07 19:12:01 +0900

    feat : Modify reset css import location

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
+import "./reset.css";
 
 ReactDOM.render(
   <React.StrictMode>
